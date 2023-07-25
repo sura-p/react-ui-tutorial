e83d58e53e00cd5d2fe9258d597d20942a38fe45
Blockchain8318 <suraj.prajapati@mail.vinove.com>
2023-07-25 14:10:44 +0530

    updated the responsiveness

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import { BrowserRouter as Router, Routes ,  Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Services from "./components/Services";
 import "./styles/App.scss"
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/services.scss";
+import "./styles/mediaquery.scss";
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/services" element={<Services />}></Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
